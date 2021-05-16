0f0fe2294657a3eb2930cce7ba7f727d7120086a
Vitaly <yacubovvs@yandex.ru>
2021-05-16 23:42:15 +0300

    Added come pins define for t-wristband battery controll driver

--- sdk/drivers/driver_battery_T-Wristband_LiLyGo.ino
+++ sdk/drivers/driver_battery_T-Wristband_LiLyGo.ino
@@ -1,3 +1,14 @@
+#define TP_PIN_PIN          33
+#define I2C_SDA_PIN         21
+#define I2C_SCL_PIN         22
+#define IMU_INT_PIN         38
+#define RTC_INT_PIN         34
+#define BATT_ADC_PIN        35
+#define VBUS_PIN            36
+#define TP_PWR_PIN          25
+#define LED_PIN             4
+#define CHARGE_PIN          32
+
 void driver_battery_setup(){
 }
 
