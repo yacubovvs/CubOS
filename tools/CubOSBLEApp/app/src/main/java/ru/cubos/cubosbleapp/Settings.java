506f9cb2155608f05dbbeed10a6d7779c7834afb
Vitaly <yacubovvs@yandex.ru>
2021-11-15 03:43:33 +0300

    Added saving settings in memomry in Android BLE App

--- tools/CubOSBLEApp/app/src/main/java/ru/cubos/cubosbleapp/Settings.java
+++ tools/CubOSBLEApp/app/src/main/java/ru/cubos/cubosbleapp/Settings.java
@@ -3,26 +3,32 @@ package ru.cubos.cubosbleapp;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.CompoundButton;
+import android.widget.SeekBar;
 import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 public class Settings extends AppCompatActivity {
 
     Switch switch_syncTime;
     Switch switch_usePedometer;
     Switch switch_autosyncOnCharge;
+    SeekBar seekBar_stepsLimit;
+    SeekBar seekBar_sleepLimit;
+    SeekBar seekBar_screenOffTime;
+    SeekBar seekBar_screenOffClock;
+
+    TextView label_stepsLimit;
+    TextView label_sleepLimit;
+    TextView label_screenOffTime;
+    TextView label_screenOffClock;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
 
         ActionBar actionBar = getSupportActionBar();
 
@@ -37,7 +43,60 @@ public class Settings extends AppCompatActivity {
         switch_usePedometer.setChecked(MainActivity.mainActivity.preferences.getBoolean("switch_usePedometer", true));
         switch_autosyncOnCharge.setChecked(MainActivity.mainActivity.preferences.getBoolean("switch_autosyncOnCharge", true));
 
+        seekBar_stepsLimit = findViewById(R.id.seekBar_stepsLimit);
+        seekBar_sleepLimit = findViewById(R.id.seekBar_sleepLimit);
+        seekBar_screenOffTime = findViewById(R.id.seekBar_screenOffTime);
+        seekBar_screenOffClock = findViewById(R.id.seekBar_screenOffClock);
+
+        label_stepsLimit = findViewById(R.id.label_stepsLimit);
+        label_sleepLimit = findViewById(R.id.label_sleepLimit);
+        label_screenOffTime = findViewById(R.id.label_screenOffTime);
+        label_screenOffClock = findViewById(R.id.label_screenOffClock);
+
+        //MainActivity.mainActivity.preferences.putInt("last_stepsLimit", stepLimit);
+
+        //setLabelStepsLimit(stepLimit);
+
         setListeners();
+
+        seekBar_stepsLimit.setProgress(MainActivity.mainActivity.preferences.getInt("last_progress_stepsLimit", 20));
+        seekBar_sleepLimit.setProgress(MainActivity.mainActivity.preferences.getInt("last_progress_sleepLimit", 40));
+        seekBar_screenOffTime.setProgress(MainActivity.mainActivity.preferences.getInt("last_progress_screenOffTime", 5));
+        seekBar_screenOffClock.setProgress(MainActivity.mainActivity.preferences.getInt("last_progress_screenOffClock", 3));
+
+    }
+
+    void setLabelStepsLimit(int stepLimit){
+        label_stepsLimit.setText("" + stepLimit + " steps");
+    }
+
+    void setLabelSleepLimit(int sleepLimit){
+        int minutes = sleepLimit%60;
+        int hours = (sleepLimit - minutes)/60;
+
+        label_sleepLimit.setText("" + hours + " hours " + minutes + " minutes");
+    }
+
+    void setLabelScreenTimeOff(int screenOffTime){
+
+        if(screenOffTime==0){
+            label_screenOffTime.setText("Never");
+        } else {
+            int seconds = screenOffTime%60;
+            int minutes = (screenOffTime - seconds)/60;
+            label_screenOffTime.setText("" + minutes + " minutes " + seconds + " seconds");
+        }
+    }
+
+
+    void setLabelScreenOffClock(int screenOffClock){
+        if(screenOffClock==0){
+            label_screenOffTime.setText("Never");
+        } else {
+            int seconds = screenOffClock%60;
+            int minutes = (screenOffClock - seconds)/60;
+            label_screenOffClock.setText("" + minutes + " minutes " + seconds + " seconds");
+        }
     }
 
     @Override
@@ -72,6 +131,111 @@ public class Settings extends AppCompatActivity {
                 MainActivity.mainActivity.preferences.putBoolean("switch_autosyncOnCharge", isChecked);
             }
         });
+
+        seekBar_stepsLimit.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int max = 30000;
+                int min = 1000;
+                int stepLimit = ((max - min)  * progress /100 + min)/500*500;
+
+                MainActivity.mainActivity.preferences.putInt("last_stepsLimit", stepLimit);
+                MainActivity.mainActivity.preferences.putInt("last_progress_stepsLimit", progress);
+                setLabelStepsLimit(stepLimit);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        seekBar_sleepLimit.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int max = 960;
+                int min = 120;
+                int sleepLimit = ((max - min)  * progress /100 + min)/10*10;
+
+                MainActivity.mainActivity.preferences.putInt("last_sleepLimit", sleepLimit);
+                MainActivity.mainActivity.preferences.putInt("last_progress_sleepLimit", progress);
+                setLabelSleepLimit(sleepLimit);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        seekBar_screenOffTime.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int max = 120;
+                int min = 1;
+                int screenOffTime = ((max - min)  * progress /100 + min);
+
+                MainActivity.mainActivity.preferences.putInt("last_progress_screenOffTime", progress);
+
+                if(progress==100){
+                    MainActivity.mainActivity.preferences.putInt("last_screenOffTime", 0);
+                    setLabelScreenTimeOff(0);
+                } else {
+                    MainActivity.mainActivity.preferences.putInt("last_screenOffTime", screenOffTime);
+                    setLabelScreenTimeOff(screenOffTime);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        seekBar_screenOffClock.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int max = 120;
+                int min = 1;
+                int screenOffTime = ((max - min)  * progress /100 + min);
+
+                MainActivity.mainActivity.preferences.putInt("last_progress_screenOffClock", progress);
+
+                if(progress==100){
+                    MainActivity.mainActivity.preferences.putInt("last_screenOffClock", 0);
+                    setLabelScreenOffClock(0);
+                } else {
+                    MainActivity.mainActivity.preferences.putInt("last_screenOffClock", screenOffTime);
+                    setLabelScreenOffClock(screenOffTime);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 
 }
